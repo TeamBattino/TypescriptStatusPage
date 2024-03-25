401bd0a90bf98030d1c1afec4253cf7eddbb3fd5
Battino <battino46@gmail.com>
2024-03-25 14:02:57 +0100

    move source code into src/ directory, add readFileAsJson function

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+import { appendFile, readFile } from "node:fs/promises";
+
+export const appendToLog = async (message: string) => {
+    let logPath : string = process.env.LOG_FILE ?? "status.log";
+    appendFile(logPath, `${message}\n`);
+}
+export const getJsonFromFile = async (path: string) => {
+    const data = await readFile(path, "utf-8");
+    return JSON.parse(data);
+}
--- utils.ts
+++
@@ -1,6 +0,0 @@
-import { appendFile } from "node:fs/promises";
-
-export const appendToLog = async (message: string) => {
-    let logPath : string = process.env.LOG_FILE ?? "status.log";
-    appendFile(logPath, `${message}\n`);
-}
